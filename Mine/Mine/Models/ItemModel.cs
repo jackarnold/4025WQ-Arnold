d03dfab52fde8137c4841ea63e48f8e43d387b77
Jack Arnold <arnoldj1@seattleu.edu>
2020-01-28 17:04:43 -0800

    Added Update Method to ItemMode

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -8,6 +8,18 @@
         // Add Unique attributes for Item
         public int Value { get; set; } = 0;
 
+        public bool Update(ItemModel data)
+        {
+            //DO NOT UPDATE THE ID, otherwise the record will be orphaned
+            //Id = data.id //leave commented out
+
+            //update the base
+            Name = data.Name;
+            Description = data.Description;
+            //update the extended
+            Value = data.Value;
+            return true;
+        }
 
 
     }
